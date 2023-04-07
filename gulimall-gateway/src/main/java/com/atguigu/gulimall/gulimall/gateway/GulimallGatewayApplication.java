8b0024cd227d6a003cb35882e834a579c7a37884
zzzk1 <36791902+zzzk1@users.noreply.github.com>
2023-04-07 12:31:40 +0800

    网关注册

---
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.gulimall.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})//排除掉数据源相关配置
+public class GulimallGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GulimallGatewayApplication.class, args);
+	}
+
+}
