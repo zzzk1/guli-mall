3e19ddce4854592d302ec69b7ce60e3412b7c66d
zzzk1 <36791902+zzzk1@users.noreply.github.com>
2023-04-07 12:30:23 +0800

    联通前端

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/CouponController.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/CouponController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,17 +26,21 @@ import com.atguigu.common.utils.R;
  * @email madzhou1@gmail.com
  * @date 2023-04-06 19:59:20
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
 
-    @RequestMapping("/member/list")
-    public R membercoupons() {    //全系统的所有返回都返回R
-        // 应该去数据库查用户对于的优惠券，但这个我们简化了，不去数据库查了，构造了一个优惠券给他返回
-        CouponEntity couponEntity = new CouponEntity();
-        couponEntity.setCouponName("满100-10");//优惠券的名字
-        return R.ok().put("coupons", Arrays.asList(couponEntity));
+    @Value("${coupon.user.name}")//从application.properties中获取//不要写user.name，他是环境里的变量
+    private String name;
+    @Value("${coupon.user.age}")
+    private Integer age;
+
+    @RequestMapping("/test")
+    public R test() {
+
+        return R.ok().put("name", name).put("age", age);
     }
 }
